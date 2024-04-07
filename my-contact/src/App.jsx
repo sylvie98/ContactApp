8fc856a3ef0749282c91e81b40ea5777340c7369
sylvie98 <sylviemasengesho5@gmail.com>
2024-04-07 13:28:21 +0200

    first commit installing vite and tailwind

---
+++ my-contact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' bg-slate-400 flex  items-center  justify-around'>
+     <h1 className="text-3xl font-bold ">Contacts</h1>
+     <a>Home</a>
+    </div>
+  )
+}
+
+export default App
+
